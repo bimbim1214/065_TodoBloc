713c11d43b4b740291970863110ec13b8791620e
bimbim1214 <bimoadityapangestu@gmail.com>
2025-05-07 19:53:02 +0700

    tambah class TodoEventAdd untuk menangani penambahan todo baru dengan title dan tanggal

--- lib/bloc/todo_event.dart
+++ lib/bloc/todo_event.dart
@@ -1,1 +1,11 @@
+part of 'todo_bloc.dart';
+
+sealed class TodoEvent {}
+
+final class TodoEventAdd extends TodoEvent {
+  final String title;
+  final DateTime date;
+  TodoEventAdd({required this.title, required this.date,});
+}
+
 
