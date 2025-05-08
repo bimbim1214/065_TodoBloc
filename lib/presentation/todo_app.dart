6e29fefa23e943f5125cf8f4436775e3cce83abb
bimbim1214 <bimoadityapangestu@gmail.com>
2025-05-08 18:53:57 +0700

    menambahkan tombol pilih tanggal dan hubungkan dengan TodoBloc

--- lib/presentation/todo_app.dart
+++ lib/presentation/todo_app.dart
@@ -33,9 +33,29 @@ class TodoPage extends StatelessWidget {
                           return Text('No date selected');
                         },
                       ),
-                      
+                      SizedBox(width: 16.0),
+                      ElevatedButton(
+                        onPressed: () {
+                          showDatePicker(
+                            context: context,
+                            initialDate: DateTime.now(),
+                            firstDate: DateTime(2000),
+                            lastDate: DateTime(2100),
+                          ).then((selectedDate) {
+                            if (selectedDate != null) {
+                              context.read<TodoBloc>().add(
+                                    TodoSelectedDate(date: selectedDate),
+                                  );
+                            }
+                          });
+                        },
+                        child: Text('Select Date'),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            ],
+              
           ),
         ),
       ),
