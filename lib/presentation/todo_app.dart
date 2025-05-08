2f89914486a105e0caa95eb975323f58be634be1
bimbim1214 <bimoadityapangestu@gmail.com>
2025-05-08 19:11:20 +0700

    menampilkan daftar todo dan state loading serta daftar kosong

--- lib/presentation/todo_app.dart
+++ lib/presentation/todo_app.dart
@@ -95,7 +95,21 @@ class TodoPage extends StatelessWidget {
                   ],
                 ),
               ),
-              
+              SizedBox(height: 16.0),
+              Expanded(
+                child: BlocBuilder<TodoBloc, TodoState>(
+                  builder: (context, state) {
+                    if (state is TodoLoading) {
+                      return Center(child: CircularProgressIndicator());
+                    } else if (state is TodoLoaded) {
+                      if (state.todos.isEmpty) {
+                        return Center(child: Text('Todo list is empty'));
+                      }
+                      
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
