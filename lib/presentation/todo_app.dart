bfb371dc3af327bb596a419196af514b36a30ad0
bimbim1214 <bimoadityapangestu@gmail.com>
2025-05-08 19:16:38 +0700

    menampilkan tanggal dan status penyelesaian pada item todo

--- lib/presentation/todo_app.dart
+++ lib/presentation/todo_app.dart
@@ -130,7 +130,25 @@ class TodoPage extends StatelessWidget {
                                       ),
                                     ),
                                     SizedBox(height: 4.0),
-                                    
+                                    Text(
+                                      '${todo.date.day}/${todo.date.month}/${todo.date.year}',
+                                      style: TextStyle(color: Colors.grey),
+                                    ),
+                                    SizedBox(height: 4.0),
+                                    Text(
+                                      todo.isCompleted
+                                          ? 'Completed'
+                                          : 'Not Completed',
+                                      style: TextStyle(
+                                        color: todo.isCompleted
+                                            ? Colors.green
+                                            : Colors.red,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                
+                  },
                 ),
               ),
             ],
