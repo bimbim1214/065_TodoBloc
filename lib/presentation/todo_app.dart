22131b73f9fb8fd1f842a57c2ac4e2903755627a
bimbim1214 <bimoadityapangestu@gmail.com>
2025-05-08 18:51:58 +0700

    menambah tanggal yang dipilih dengan Bloc di halaman Todo

--- lib/presentation/todo_app.dart
+++ lib/presentation/todo_app.dart
@@ -9,6 +9,36 @@ class TodoPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final _key = GlobalKey<FormState>();
     final _controller = TextEditingController();
-    
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text('Todo List'),
+              Row(
+                children: [
+                  Column(
+                    children: [
+                      Text('Selected Date'),
+                      BlocBuilder<TodoBloc, TodoState>( 
+                        builder: (context, state) {
+                          if (state is TodoLoaded) {
+                            if (state.selectedDate != null) {
+                              return Text(
+                                '${state.selectedDate!.day}/${state.selectedDate!.month}/${state.selectedDate!.year}',
+                              );
+                            }
+                          }
+                          return Text('No date selected');
+                        },
+                      ),
+                      
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
